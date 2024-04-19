0d7ae11985ba56076038b96650b11b74845ce026
Pranav Jadhav <trinitypranav@gmail.com>
2024-04-19 12:14:07 +0530

    types exercise

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,18 @@ let sales: number = 123_456;
 let course: string = "LearnTypescript";
 let isCoding: boolean = true;
 let level; // typescript assumes this is of type 'any'
+
+// number
+let a = 100;
+// string
+let b = 'Coffee';
+// boolean[]
+let c = [true, false, false];
+// {age: 20}
+let d = {age: 20};
+// number[]
+let e = [3];
+// any
+let f;
+// any[]
+let g = [];
